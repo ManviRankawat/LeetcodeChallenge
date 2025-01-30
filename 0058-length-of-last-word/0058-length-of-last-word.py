e3929e61e855e8840567c88af666d51a790521dd
Manvi Rankawat <81751478+ManviRankawat@users.noreply.github.com>
2025-01-30 17:57:35 -0500

    Time: 4 ms (5.52%), Space: 12.4 MB (56.52%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s.split():
+            return len(s.split()[-1])
+        return 0
+        
+        
