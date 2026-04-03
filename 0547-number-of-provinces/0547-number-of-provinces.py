a5b6e8fe80b8432a32d2eea2eb31044612a0da5c
Manvi Rankawat <81751478+ManviRankawat@users.noreply.github.com>
2026-04-03 17:29:58 -0400

    Time: 4 ms (90.46%), Space: 12.9 MB (76.81%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findCircleNum(self, isConnected):
+        """
+        :type isConnected: List[List[int]]
+        :rtype: int
+        """
+        size = len(isConnected)
+        numberOfComponents = 0
+        visit = [False] * size
+
+        for i in range(size):
+            if not visit[i]:
+                numberOfComponents += 1
+                self.dfs(i, isConnected, visit)
+
+        return numberOfComponents
+
+    def dfs(self, node, isConnected, visit):
+        visit[node] = True
+        for i in range(len(isConnected)):
+            if isConnected[node][i] and not visit[i]:
+                self.dfs(i, isConnected, visit)
